45f56ece9a674043d206249d6c077334bc90f687
Jeffrey Berry <jeffreyberry93@gmail.com>
2017-03-08 21:49:54 -0800

    feat(webpack-dev-server): got a simple dev server going

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 'use strict';
 
-import webpack from 'webpack';
+const path = require('path');
+const webpack = require('webpack');
 
 const config = {
-
+  entry: './app/src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: '/app/dist/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./"),
+    compress: true,
+    port: 9000
+  }
 };
 
 module.exports = config;
