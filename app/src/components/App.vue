dcef8d066060584027a7012b2158797015ec3166
jberry93 <jeffreyberry93@gmail.com>
2017-03-10 23:18:15 -0800

    feat(vue): initialized a simple vue instance

---
+++ app/src/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="vue">Hello from App.vue</h1>
+</template>
+
+<script>
+  console.info('App.vue loaded!');
+</script>
+
+<style>
+  .vue {
+    text-align: center;
+  }
+</style>
